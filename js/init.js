cba5d209043fc44272311870845e0fde49e94c13
basedeswpro801 <88286689+basedeswpro801@users.noreply.github.com>
2021-08-02 12:18:59 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCZL47152148R0VGhLOGcChPoNPtnSYxDU",
+    authDomain: "basedeswpro801.firebaseapp.com",
+    projectId: "basedeswpro801",
+    storageBucket: "basedeswpro801.appspot.com",
+    messagingSenderId: "41090835615",
+    appId: "1:41090835615:web:bb23d0e9da4f2074f1aeaa",
+    measurementId: "G-F261PFDVRJ"
+});
